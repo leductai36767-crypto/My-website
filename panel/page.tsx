d2e2a72f5dc1e93db0c46fd49da8082a807bbd4d
Hoàng Huy <218056340+neotwis@users.noreply.github.com>
2026-09-26 20:00:58 +0700

    Create page.tsx

---
+++ panel/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  // Thay URL bên dưới bằng địa chỉ bạn muốn chuyển hướng đến
+  redirect("https://panel-astoncloud.vercel.app/");
+}
