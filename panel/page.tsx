d636575121e2513875f736f06121499f644260e7
Hoàng Huy <218056340+neotwis@users.noreply.github.com>
2026-09-26 20:01:41 +0700

    Delete panel directory

--- panel/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Page() {
-  // Thay URL bên dưới bằng địa chỉ bạn muốn chuyển hướng đến
-  redirect("https://panel-astoncloud.vercel.app/");
-}
